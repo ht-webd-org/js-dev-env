147876e2ce9c6d0ad1f29237970721497ab9182a
helena <20523120+helenacodes@users.noreply.github.com>
2016-12-20 00:27:30 +0000

    Set up ESLint watch

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const houseValue = numeral(1000).format('$0,0.00');
-console.log(`This house is worth ${houseValue}`);
+console.log(`This house is worth ${houseValue}`); // eslint-disable-line no-console
