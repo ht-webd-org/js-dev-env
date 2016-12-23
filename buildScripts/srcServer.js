4347a57e534085271da2fb5a196452637248551c
helena <20523120+helenacodes@users.noreply.github.com>
2016-12-23 01:00:09 +0000

    Added centralized HTTP requests with Fetch

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -19,6 +19,15 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../src/index.html'));
 })
 
+app.get('/users', function(req, res) {
+   // Temporary hardcoded data
+   res.json([
+        {"id": 1, "firstName": "Mickey", "lastName": "Mouse", "email": "mickey@disney.com"},
+        {"id": 2, "firstName": "Minnie", "lastName": "Mouse", "email": "minnie@disney.com"},
+        {"id": 3, "firstName": "Donald", "lastName": "Duck", "email": "donald@disney.com"}
+    ]);
+});
+
 app.listen(port, function(err){
     if (err) {
         console.log(err);
