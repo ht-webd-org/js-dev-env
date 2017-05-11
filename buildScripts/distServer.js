0dfdae1754aa9e7db99d2ccad1361d4e454e722c
helena <20523120+helenacodes@users.noreply.github.com>
2017-05-11 14:19:14 -0700

    Add automated API deploy with Heroku

--- buildScripts/distServer.js
+++ buildScripts/distServer.js
@@ -13,15 +13,6 @@ app.use(express.static('dist'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../dist/index.html'));
-})
-
-app.get('/users', function(req, res) {
-   // Temporary hardcoded data
-   res.json([
-        {"id": 1, "firstName": "Mickey", "lastName": "Mouse", "email": "mickey@disney.com"},
-        {"id": 2, "firstName": "Minnie", "lastName": "Mouse", "email": "minnie@disney.com"},
-        {"id": 3, "firstName": "Donald", "lastName": "Duck", "email": "donald@disney.com"}
-    ]);
 });
 
 app.listen(port, function(err){
@@ -30,4 +21,4 @@ app.listen(port, function(err){
     } else {
         open('http://localhost:' + port);
     }
-})
+});
